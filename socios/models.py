1e9c89c5b146f24058b422fb3241b67f6a49d2a5
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-18 00:24:03 +0300

    add user post

--- socios/models.py
+++ socios/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
   caption = models.CharField(max_length=200)
   comment = models.TextField(blank=True)
 
+
+
   def __str__(self):
     return self.name + " | " + str(self.author)
+    
 
