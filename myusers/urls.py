876be72d1a14e32ca844767eb55bbc1ce6c650af
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-19 00:03:27 +0300

    add users log in and sign up

--- myusers/urls.py
+++ myusers/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 
 
 urlpatterns = [
-     path('', Homev.as_view(),name='home'),
-     path('post/<int:pk>', PostD.as_view(),name='postdetail'),
-     path('add_post/',AddP.as_view(),name='add-post'),
+     
 ]
