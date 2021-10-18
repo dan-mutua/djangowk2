dda3c71600e7085e327a4923db2877b131e2aff1
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-18 14:21:14 +0300

    add home page

--- socios/models.py
+++ socios/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-import uuid
 from django.urls import reverse
 
 
 
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
-# Create your models here.
-class Postm(models.Model):
- user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='content_owner')
-file = models.FileField(upload_to=user_directory_path)
 
 
 class Post(models.Model):
@@ -22,7 +14,6 @@ class Post(models.Model):
   name = models.CharField(max_length=200)
   caption = models.CharField(max_length=200)
   comment = models.TextField(blank=True)
-  content = models.ManyToManyField(Postm,  related_name='contents')
   posttime=models.DateTimeField(auto_now_add=True)
   likes = models.IntegerField(default=0)
 
@@ -32,9 +23,5 @@ def __str__(self):
     return self.name + " | " + str(self.author)
     
   
-def get_absolute_url(self):
-		return reverse('postdetails', args=[str(self.id)])
 
-def __str__(self):
-		return str(self.id)  
 
