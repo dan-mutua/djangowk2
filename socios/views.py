8aee19058766bfdb9a54bef27de918eed05a727b
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-18 14:16:40 +0300

    add home page

--- socios/views.py
+++ socios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+
+from socios.models import Post
 
 # Create your views here.
-def home(request):
-  return render(request, 'home.html', {})
+class  Home(ListView):
+  model = Post
+  template_name='home.html'
